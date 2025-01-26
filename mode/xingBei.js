b69ff9bdb31f349afbf544c78946bbf32fc205a5
NongJie <245722665@qq.com>
2025-01-26 21:10:20 +0800

    使事件即使在无相关时机的技能下，也能filterStop

--- mode/xingBei.js
+++ mode/xingBei.js
@@ -6389,6 +6389,148 @@ export default () => {
 				},
 			},
 			event:{
+				trigger(name) {
+					if (_status.video) return;
+					if (!_status.gameDrawed && ["lose", "gain", "loseAsync", "equip", "addJudge", "addToExpansion"].includes(this.name)) return;
+					if (name === "gameDrawEnd") _status.gameDrawed = true;
+					if (name === "gameStart") {
+						lib.announce.publish("Noname.Game.Event.GameStart", {});
+						lib.announce.publish("gameStart", {});
+						if (_status.brawl && _status.brawl.gameStart) _status.brawl.gameStart();
+						if (lib.config.show_cardpile) ui.cardPileButton.style.display = "";
+						_status.gameStarted = true;
+						game.showHistory();
+					}
+					const event = this;
+					if (event.filterStop && event.filterStop()) return;
+					if (!lib.hookmap[name] && !lib.config.compatiblemode) return;
+					if (!game.players || !game.players.length) return;
+					let start = [_status.currentPhase, event.source, event.player, game.me, game.players[0]].find(i => get.itemtype(i) == "player");
+					if (!start) return;
+					if (!game.players.includes(start) && !game.dead.includes(start)) start = game.findNext(start);
+					const firstDo = {
+						player: "firstDo",
+						todoList: [],
+						doneList: [],
+					};
+					const lastDo = {
+						player: "lastDo",
+						todoList: [],
+						doneList: [],
+					};
+					const doingList = [];
+					const roles = ["player", "source", "target", "global"];
+					const playerMap = game.players.concat(game.dead).sortBySeat(start);
+					let player = start;
+					let allbool = false;
+					do {
+						const doing = {
+							player: player,
+							todoList: [],
+							doneList: [],
+							listAdded: {},
+							addList(skill) {
+								if (!skill) return;
+								if (Array.isArray(skill)) return skill.forEach(i => this.addList(i));
+								if (this.listAdded[skill]) return;
+								this.listAdded[skill] = true;
+			
+								const info = lib.skill[skill];
+								const list = info.firstDo ? firstDo.todoList : info.lastDo ? lastDo.todoList : this.todoList;
+								if (typeof info.getIndex === "function") {
+									const indexedResult = info.getIndex(event, player, name);
+									if (Array.isArray(indexedResult)) {
+										indexedResult.forEach(indexedData => {
+											list.push({
+												skill: skill,
+												player: this.player,
+												priority: get.priority(skill),
+												indexedData,
+											});
+										});
+									} else if (typeof indexedResult === "number" && indexedResult > 0) {
+										for (let i = 0; i < indexedResult; i++) {
+											list.push({
+												skill: skill,
+												player: this.player,
+												priority: get.priority(skill),
+												indexedData: true,
+											});
+										}
+									}
+								} else {
+									list.push({
+										skill: skill,
+										player: this.player,
+										priority: get.priority(skill),
+									});
+								}
+								if (typeof list.player == "string") list.sort((a, b) => b.priority - a.priority || playerMap.indexOf(a) - playerMap.indexOf(b));
+								else list.sort((a, b) => b.priority - a.priority);
+								allbool = true;
+							},
+						};
+			
+						const notemp = player.skills.slice();
+						for (const j in player.additionalSkills) {
+							if (!j.startsWith("hidden:")) notemp.addArray(player.additionalSkills[j]);
+						}
+						Object.keys(player.tempSkills)
+							.filter(skill => {
+								if (notemp.includes(skill)) return false;
+								const expire = player.tempSkills[skill];
+								if (typeof expire === "function") return expire(event, player, name);
+								if (get.objtype(expire) === "object")
+									return roles.some(role => {
+										if (role !== "global" && player !== event[role]) return false;
+										if (Array.isArray(expire[role])) return expire[role].includes(name);
+										return expire[role] === name;
+									});
+							})
+							.forEach(skill => {
+								delete player.tempSkills[skill];
+								player.removeSkill(skill);
+							});
+			
+						if (lib.config.compatiblemode) {
+							doing.addList(
+								game.expandSkills(player.getSkills("invisible").concat(lib.skill.global)).filter(skill => {
+									const info = get.info(skill);
+									if (!info || !info.trigger) return false;
+									return roles.some(role => {
+										if (info.trigger[role] === name) return true;
+										if (Array.isArray(info.trigger[role]) && info.trigger[role].includes(name)) return true;
+									});
+								})
+							);
+						} else
+							roles.forEach(role => {
+								doing.addList(lib.hook.globalskill[role + "_" + name]);
+								doing.addList(lib.hook[player.playerid + "_" + role + "_" + name]);
+							});
+						delete doing.listAdded;
+						delete doing.addList;
+						doingList.push(doing);
+						player = player.nextSeat;
+					} while (player && player !== start);
+					doingList.unshift(firstDo);
+					doingList.push(lastDo);
+					// console.log(name,event.player,doingList.map(i=>({player:i.player,todoList:i.todoList.slice(),doneList:i.doneList.slice()})))
+			
+					if (allbool) {
+						const next = game.createEvent("arrangeTrigger", false, event);
+						next.setContent("arrangeTrigger");
+						next.doingList = doingList;
+						next._trigger = event;
+						next.triggername = name;
+						next.playerMap = playerMap;
+						event._triggering = next;
+						next.then(() => (event._triggering = void 0));
+						return next;
+					}
+					return null;
+				},
+
 				/**
 				 * @param {num} num 伤害改变量 
 				 */
