84ccf93daf30fb2e859a8ac5c9b26de1024578c0
NongJie <245722665@qq.com>
2025-02-23 21:29:31 +0800

    优化技能在出牌记录栏中头图文字显示

--- mode/xingBei.js
+++ mode/xingBei.js
@@ -2346,6 +2346,148 @@ export default () => {
 				});
 				if(game.zhanJiList) game.zhanJiList['moDanFangXiang']='you';
 			},
+
+
+			/**
+			 * @param { Player } player
+			 * @param { string | Card[] } card
+			 * @param { Player[] } [targets]
+			 * @param { GameEventPromise } [event]
+			 * @param { boolean } [forced]
+			 * @param { string } [logvid]
+			 */
+			logv(player, card, targets, event, forced, logvid) {
+				if (!player) {
+					player = _status.event.getParent().logvid;
+					if (!player) return;
+				}
+				const node = ui.create.div(".hidden");
+				node.node = {};
+				logvid = logvid || get.id();
+				game.broadcast(game.logv, player, card, targets, event, forced, logvid);
+				if (typeof player == "string") {
+					const childNode = Array.from(ui.historybar.childNodes).find(value => value.logvid == player);
+					if (childNode) childNode.added.push(card);
+					return;
+				}
+				if (typeof card == "string") {
+					if (card != "die") {
+						if (lib.skill[card] && lib.skill[card].logv === false && !forced) return;
+						if (!lib.translate[card]) return;
+					}
+					let avatar;
+					if (!player.isUnseen(0)) avatar = player.node.avatar.cloneNode();
+					else if (!player.isUnseen(1)) avatar = player.node.avatar2.cloneNode();
+					else return;
+					node.node.avatar = avatar;
+					avatar.style.transform = "";
+					avatar.className = "avatar";
+					if (card == "die") {
+						node.dead = true;
+						node.player = player;
+						const avatar2 = avatar.cloneNode();
+						avatar2.className = "avatarbg grayscale1";
+						avatar.appendChild(avatar2);
+						avatar.style.opacity = 0.6;
+					} else {
+						var str=get.translation(card, "skill");
+						var reg=new RegExp(/[\[\(\)].{1,5}[\]\)]/g,'g');
+						if(str.replace){
+							str=str.replace(reg,'');
+						}
+						str=str.slice(0, 4);
+						node.node.text = ui.create.div("", str, avatar);
+						node.node.text.dataset.nature = "water";
+						node.skill = card;
+					}
+					node.appendChild(avatar);
+					if (card == "die" && targets && targets != player) {
+						node.source = targets;
+						player = targets;
+						if (!player.isUnseen(0)) avatar = player.node.avatar.cloneNode();
+						else if (!player.isUnseen(1)) avatar = player.node.avatar2.cloneNode();
+						else return;
+						avatar.style.transform = "";
+						node.node.avatar2 = avatar;
+						avatar.classList.add("avatar2");
+						node.appendChild(avatar);
+					}
+				} else if (Array.isArray(card)) {
+					node.cards = card[1].slice(0);
+					card = card[0];
+					const info = [card.xiBie || "", card.mingGe || "", card.name || "", card.duYou || ""];
+					if (!Array.isArray(node.cards) || !node.cards.length) node.cards = [ui.create.card(node, "noclick", true).init(info)];
+
+					if (card.copy) card.copy(node, false);
+					else {
+						card = ui.create.card(node, "noclick", true);
+						card.init(info);
+					}
+					let avatar;
+					if (!player.isUnseen(0)) avatar = player.node.avatar.cloneNode();
+					else if (!player.isUnseen(1)) avatar = player.node.avatar2.cloneNode();
+					else return;
+					node.node.avatar = avatar;
+					avatar.style.transform = "";
+					avatar.classList.add("avatar2");
+					node.appendChild(avatar);
+					if (targets && targets.length == 1 && targets[0] != player && get.itemtype(targets[0]) == "player")
+						(() => {
+							let avatar2;
+							const target = targets[0];
+							if (!target.isUnseen(0)) avatar2 = target.node.avatar.cloneNode();
+							else if (!player.isUnseen(1)) avatar2 = target.node.avatar2.cloneNode();
+							else return;
+							node.node.avatar2 = avatar2;
+							avatar2.style.transform = "";
+							avatar2.classList.add("avatar2");
+							avatar2.classList.add("avatar3");
+							node.insertBefore(avatar2, avatar);
+						})();
+				}
+				if (targets && targets.length) {
+					if (targets.length == 1 && targets[0] == player) {
+						node.targets = [];
+					} else {
+						node.targets = targets;
+					}
+				}
+				const fullheight = ui.historybar.offsetHeight,
+					num = Math.round((fullheight - 8) / 50),
+					margin = (fullheight - 42 * num) / (num + 1);
+				node.style.transform = "scale(0.8)";
+				ui.historybar.insertBefore(node, ui.historybar.firstChild);
+				ui.refresh(node);
+				node.classList.remove("hidden");
+				Array.from(ui.historybar.childNodes).forEach((value, index) => {
+					if (index < num) {
+						value.style.transform = `scale(1) translateY(${margin + index * (42 + margin) - 4}px)`;
+						return;
+					}
+					if (value.removetimeout) return;
+					value.style.opacity = 0;
+					value.style.transform = `scale(1) translateY(${fullheight}px)`;
+					value.removetimeout = setTimeout(
+						(
+							current => () =>
+								current.remove()
+						)(value),
+						500
+					);
+				});
+				if (lib.config.touchscreen) node.addEventListener("touchstart", ui.click.intro);
+				else {
+					node.addEventListener(lib.config.pop_logv ? "mousemove" : "click", ui.click.logv);
+					node.addEventListener("mouseleave", ui.click.logvleave);
+				}
+				node.logvid = logvid;
+				node.added = [];
+				if (!game.online) {
+					event = event || _status.event;
+					event.logvid = node.logvid;
+				}
+				return node;
+			}
 		},
 		
 
