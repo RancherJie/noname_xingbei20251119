59b1ed3629dabb887420e92d7ec58677775d6046
NongJie <245722665@qq.com>
2025-03-12 13:00:07 +0800

    更新1.0.4

--- game/update.js
+++ game/update.js
@@ -1,13 +1,12 @@
 window.noname_update = {
-	version: "1.0.3",
-	update: "1.0.3", //新版本更新文件较多，直接强制进行全量更新
+	version: "1.0.4",
+	update: "1.0.4", //新版本更新文件较多，直接强制进行全量更新
 	changeLog: [
-		'修复恶魔之魂错误增伤',
-        '修复弃置盖牌未为gaiPai赋值',
-        '创建扩展时扩展文件默认使用game.import方法',
-        '修复FAQ魔法入门无法触发Magic！',
-        '优化触屏情况下6人多控布局',
-        '修复魔弹和中毒未正确初始化',
+
 	],
-	files: [],
+	files: [
+		'game/update.js',
+		'mode/xingBei.js',
+		'character/shenZiChuangLin.js',
+	],
 };
