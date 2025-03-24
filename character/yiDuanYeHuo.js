bc93b25f5dce06975a5ff4b172c0b8c4ac9b2fb9
NongJie <245722665@qq.com>
2025-03-24 15:44:20 +0800

    shangHaiAfter时机替换回damgaeAfter

--- character/yiDuanYeHuo.js
+++ character/yiDuanYeHuo.js
@@ -499,7 +499,7 @@ game.import('character',function(lib,game,ui,get,ai,_status){
                 }
             },
             jingHuaZhiShu:{
-                trigger:{player:'shangHaiAfter'},
+                trigger:{player:'damageAfter'},
                 filter:function(event,player){
                     return player.countCards('h')>0;
                 },
