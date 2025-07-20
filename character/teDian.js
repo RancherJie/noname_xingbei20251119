4148ac2d43dbda8d306c888a220b4f0a5d3ed178
NongJie <245722665@qq.com>
2025-07-20 10:35:37 +0800

    修复宝石少女三秒原则无法发动

--- character/teDian.js
+++ character/teDian.js
@@ -935,7 +935,7 @@ game.import('character',function(lib,game,ui,get,ai,_status){
                 group: 'sanMiaoYuanZe_discard',
                 subSkill:{
                     discard:{
-                        trigger:{global: "loseBefore"},
+                        trigger:{global: "loseEnd"},
                         direct: true,
                         filter: function (event, player) {
                             return event.player.side==player.side&&event.player!=player &&((event.type=='discard'&&(event.getParent().showCards||event.getParent().showHiddenCards)) || (event.type=='use'));
