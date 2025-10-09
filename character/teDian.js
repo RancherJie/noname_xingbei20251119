ffd1359d2c34ce65c9bca5890a53d155fcf0e61a
NongJie <245722665@qq.com>
2025-10-09 10:04:29 +0800

    修复圣仲裁者天平倾倒和神之审判会触发圣方之右

--- character/teDian.js
+++ character/teDian.js
@@ -3119,9 +3119,9 @@ game.import('character',function(lib,game,ui,get,ai,_status){
                     var zuo_cards=player.getExpansions('tianPing_zuo');
                     var you_cards=player.getExpansions('tianPing_you');
                     if(zuo_cards.length>you_cards.length){
-                        await player.discard(zuo_cards,'tianPing_zuo');
+                        await player.discard(zuo_cards,'tianPing_zuo','sheQi');
                     }else{
-                        await player.discard(you_cards,'tianPing_you');
+                        await player.discard(you_cards,'tianPing_you','sheQi');
                     }
                     var cards=get.cards(4);
                     await player.showHiddenCards(cards);
@@ -3151,8 +3151,8 @@ game.import('character',function(lib,game,ui,get,ai,_status){
                 content: async function(event, trigger, player) {
                     var zuo_cards=player.getExpansions('tianPing_zuo');
                     var you_cards=player.getExpansions('tianPing_you');
-                    await player.discard(zuo_cards,'tianPing_zuo');
-                    await player.discard(you_cards,'tianPing_you');
+                    await player.discard(zuo_cards,'tianPing_zuo','sheQi');
+                    await player.discard(you_cards,'tianPing_you','sheQi');
                     var targets=await player.chooseTarget('神之审判：对2名目标对手各造成2点攻击伤害③',2,lib.filter.opponent,true).set('ai',function(target){
                         return get.damageEffect2(target, _status.event.player, 2);
                     }).forResultTargets();
@@ -3719,9 +3719,9 @@ game.import('character',function(lib,game,ui,get,ai,_status){
             shenEBiJi:"[响应]善恶必计",
             shenEBiJi_info:"[回合限定]<span class='tiaoJian'>(我方角色承受伤害⑥并结算完毕后，你将手牌中1张基础牌面朝上[展示]放到【天平·左】或【天平·右】上)</span>你摸1张牌[强制]。",
             tianPingQingDao:"[被动]天平倾倒",
-            tianPingQingDao_info:"<span class='tiaoJian'>(你的回合开始时或你的回合结束时，若【天平·左】和【天平·右】上的牌差>1，移除牌数多的一方上所有牌)</span>展示牌堆顶4张牌[展示]，你将其中2张加入手牌[强制]并弃掉另外2张牌，你+1<span class='hong'>【罪】</span>，然后目标队友+2[治疗]。",
+            tianPingQingDao_info:"<span class='tiaoJian'>(你的回合开始时或你的回合结束时，若【天平·左】和【天平·右】上的牌差>1，舍弃牌数多的一方上所有牌)</span>展示牌堆顶4张牌[展示]，你将其中2张加入手牌[强制]并弃掉另外2张牌，你+1<span class='hong'>【罪】</span>，然后目标队友+2[治疗]。",
             shenZhiShenPan:"[响应]神之审判",
-            shenZhiShenPan_info:"<span class='tiaoJian'>(你的回合结束时，若【天平·左】和【天平·右】上的牌数都>2)</span>移除【天平·左】与【天平·右】上的所有牌，然后对2名目标角色各造成2点攻击伤害③。",
+            shenZhiShenPan_info:"<span class='tiaoJian'>(你的回合结束时，若【天平·左】和【天平·右】上的牌数都>2)</span>舍弃【天平·左】与【天平·右】上的所有牌，然后对2名目标角色各造成2点攻击伤害③。",
             tianPing:"(专)天平",
             tianPingZuo:"天平·左",
             tianPingYou:"天平·右",
@@ -3729,11 +3729,11 @@ game.import('character',function(lib,game,ui,get,ai,_status){
             tianPing_you:"[被动]圣方之右",
             tianPing_fangZhi:"[被动]替罪羔羊",
             tianPing_info:`<span class='greentext'>[被动]替罪羔羊</span><br>
-            <span class='tiaoJian'>(每当有新牌将被放置在本卡上时，若该卡与本卡上的牌不同系)</span>弃掉本卡上所有卡，你+1<span class='hong'>【罪】</span>。<br>
+            <span class='tiaoJian'>(每当有新牌将被放置在本卡上时，若该卡与本卡上的牌不同系)</span>移除本卡上所有卡，你+1<span class='hong'>【罪】</span>。<br>
             <span class='greentext'>[被动]罪灭之左</span><br>
             <span class='tiaoJian'>(每当本卡上的牌数增加，且增加后本卡上的牌数>1)</span>对(X-1)名目标对手造成1点攻击伤害③，X为本卡上的牌数。<br>
             <span class='greentext'>[被动]圣方之右</span><br>
-            <span class='tiaoJian'>(每当本卡上的牌被弃掉且弃牌数>1)</span>你+1[治疗]。
+            <span class='tiaoJian'>(每当本卡上的牌被移除且移除牌数>1)</span>你+1[治疗]。
             `,
             tianZui:"[启动]天罪", 
             tianZui_info:"[水晶]你+1<span class='hong'>【罪】</span>。[横置]持续到本回合结束时，你主动攻击命中时②，可将手牌中2张基本牌面朝上[展示]分别放到【天平·左】和【天平·右】上。【天罪】的效果结束时[重置]。",
