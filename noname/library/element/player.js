4bdd446ad740a60093a91bbf34ae5397f768175d
NongJie <245722665@qq.com>
2025-10-10 15:18:52 +0800

    新增玩家函数chooseButtonCard，选button和选牌，暂无角色使用

--- noname/library/element/player.js
+++ noname/library/element/player.js
@@ -5459,6 +5459,42 @@ export class Player extends HTMLDivElement {
 		next._args = Array.from(arguments);
 		return next;
 	}
+	chooseButtonCard(choose) {
+		var next = game.createEvent("chooseButtonCard");
+		next.player = this;
+		if (arguments.length == 1) {
+			for (var i in choose) {
+				next[i] = choose[i];
+			}
+		}
+		if (typeof next.filterButton == "object") {
+			next.filterButton = get.filter(next.filterButton);
+		}
+		if (typeof next.filterCard == "object") {
+			next.filterCard = get.filter(next.filterCard);
+		}
+		if (next.filterButton == undefined || next.filterButton === true) {
+			next.filterButton = lib.filter.filterButton;
+		}
+		if (next.selectButton == undefined) {
+			next.selectButton = 1;
+		}
+		if (next.filterCard == undefined || next.filterCard === true) {
+			next.filterCard = lib.filter.all;
+		}
+		if (next.selectCard == undefined) {
+			next.selectCard = 1;
+		}
+		if (next.ai1 == undefined)
+			next.ai1 = function () {
+				return 1;
+			};
+		if (next.ai2 == undefined) next.ai2 = get.unuseful2;
+		next.setContent("chooseButtonCard");
+		next._args = Array.from(arguments);
+		return next;
+	}
+
 	discardPlayerCard() {
 		var next = game.createEvent("discardPlayerCard");
 		next.player = this;
